776c82f935dc054065aa31ea4f9342ab5a012982
Daniel Kiss <littlesnorrboy@gmail.com>
2023-03-10 13:52:19 +0100

    bootstrap topics

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-use bevy::{prelude::*, window::WindowMode};
+use bevy::{ecs::query::QueryEntityError, prelude::*, window::WindowMode};
 
 pub const LAUNCHER_TITLE: &str = "L-branches";
+
+#[derive(Component)]
+struct Vertex {
+    pub id: u32,
+}
+
+#[derive(Resource)]
+struct NextId(pub u32);
+
+#[derive(Resource)]
+struct Spline {
+    nodes: Vec<(u32, Vec3)>,
+}
 
 pub fn app(fullscreen: bool) -> App {
     let mode = if fullscreen {
@@ -19,14 +32,62 @@ pub fn app(fullscreen: bool) -> App {
         }),
         ..default()
     }))
-    .add_startup_system(setup);
+    .insert_resource(NextId(0))
+    .insert_resource(Spline { nodes: vec![] })
+    .add_startup_system(setup)
+    .add_system(update_spline);
     app
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut next_id: ResMut<NextId>,
+) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("leaf.png"),
-        ..default()
-    });
+
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("topic.png"),
+            transform: Transform::from_translation(Vec3::new(
+                -200.0, -50.0, 0.0,
+            )),
+            ..default()
+        })
+        .insert(Vertex { id: 0 });
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("topic.png"),
+            transform: Transform::from_translation(Vec3::new(200.0, 50.0, 0.0)),
+            ..default()
+        })
+        .insert(Vertex { id: 1 });
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("topic.png"),
+            transform: Transform::from_translation(Vec3::new(
+                150.0, 250.0, 0.0,
+            )),
+            ..default()
+        })
+        .insert(Vertex { id: 2 });
+
+    next_id.0 = 3;
+}
+
+fn update_spline(
+    q: Query<(&GlobalTransform, &Vertex)>,
+    change: Query<(), Changed<Vertex>>,
+    mut spline: ResMut<Spline>,
+) {
+    // if any vertex changes update the whole spline
+    if change.iter().next().is_none() {
+        return;
+    }
+    info!("Updating spline");
+    spline.nodes.clear();
+    spline
+        .nodes
+        .extend(q.iter().map(|(tr, v)| (v.id, tr.translation())));
+    spline.nodes.sort_unstable_by_key(|(id, _)| *id);
 }
