ca92464284bd3896080d9664e6e8700b29c47ed4
Daniel Kiss <littlesnorrboy@gmail.com>
2023-03-10 11:20:43 +0100

    Setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::{prelude::*, window::WindowMode};
 
-pub const LAUNCHER_TITLE: &str = "Bevy Shell - Template";
+pub const LAUNCHER_TITLE: &str = "L-branches";
 
 pub fn app(fullscreen: bool) -> App {
     let mode = if fullscreen {
@@ -10,23 +10,23 @@ pub fn app(fullscreen: bool) -> App {
     };
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        window: WindowDescriptor {
+        primary_window: Some(Window {
             title: LAUNCHER_TITLE.to_string(),
             canvas: Some("#bevy".to_string()),
             fit_canvas_to_parent: true,
             mode,
             ..default()
-        },
+        }),
         ..default()
     }))
-    .add_startup_system(load_icon);
+    .add_startup_system(setup);
     app
 }
 
-fn load_icon(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("bevy.png"),
+        texture: asset_server.load("leaf.png"),
         ..default()
     });
 }
